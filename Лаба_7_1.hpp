07fdc420eef67628f90336eda81ba944f724f36f
Nyul <Vasyl.Niul.IT.2021@lpnu.ua>
2021-11-25 14:44:50 +0200

    SSa

---
+++ Лаба_7_1.hpp
@@ -0,0 +1,13 @@
+//
+//  Лаба_7_1.hpp
+//  Laboratorna_7.1
+//
+//  Created by apple on 19.11.2021.
+//
+
+#ifndef _____7_1_hpp
+#define _____7_1_hpp
+
+#include <stdio.h>
+
+#endif /* _____7_1_hpp */
